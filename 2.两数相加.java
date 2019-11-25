abb04e7f453f7cd088d418a6c77d122fd6a9c3e5
aiglass <437307537@qq.com>
2019-11-25 22:42:08 +0800

    two sum

--- 2.两数相加.java
+++ 2.两数相加.java
@@ -14,25 +14,24 @@
  * }
  */
 class Solution {
-    int crr=0;
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-       ListNode newList = new ListNode(0);
-       ListNode a = l1, b = l2, curr = newList;
+       ListNode dummy = new ListNode(0);
+       ListNode curr =  dummy;
        int carry = 0;
-       while(a != null || b != null) {
-           int x = (a != null)?a.val:0;
-           int y = (b != null)?b.val:0;
-           int sum = carry + x + y;
-           carry = sum / 10;
+       while(l1 != null || l2 != null) {
+           int sum = carry;
+           sum = (l1 != null) ? sum+l1.val : sum;
+           sum = (l2 != null) ? sum+l2.val : sum;
            curr.next = new ListNode(sum%10);
+           carry = sum / 10;
+           l1 = l1 == null ? null : l1.next;
+           l2 = l2 == null ? null : l2.next;
            curr = curr.next;
-           if (a != null) a = a.next;
-           if (b != null) b = b.next;
        }
-       if(carry > 0){
+       if(carry != 0){
            curr.next =new ListNode(carry);
        }
-       return newList.next;
+       return dummy.next;
     }
 }
 // @lc code=end
