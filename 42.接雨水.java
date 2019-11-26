dc2149450dca326b85ad90b3468ca3b7c54de9cc
aiglass <437307537@qq.com>
2019-11-26 22:16:50 +0800

    trapping rain water

---
+++ 42.接雨水.java
@@ -0,0 +1,40 @@
+import java.util.stream.Stream;
+
+/*
+ * @lc app=leetcode.cn id=42 lang=java
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+class Solution {
+    public int trap(int[] height) {
+        int water =0 ;
+        int max = 0;//最高点的值
+        int index = 0;//最高点的位置
+        //1.找最高点
+        for(int i = 0; i < height.length; i++){
+            if(max < height[i]){
+                max = height[i];
+                index = i;
+            }
+        }
+
+        //左右两边夹击，遍历最高点
+        int pre = 0;//记录移动的位置
+        boolean flage = false;
+        for ( int i = 0; i < index; i++){
+            if(height[i]<height[pre]) water += (height[pre] - height[i]);
+            else pre = i;
+        }
+        flage = false;
+        pre = height.length - 1;
+        for ( int i = height.length -2; i > index; i--){
+            if(height[i]<height[pre]) water += (height[pre] - height[i]);
+            else pre = i;
+        }
+        return water;
+    }
+}
+// @lc code=end
+
